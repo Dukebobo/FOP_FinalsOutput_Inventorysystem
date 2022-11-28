0281314c2b237ed77c7a662674f140b45876ac46
Duke Hsu <116319266+Dukebobo@users.noreply.github.com>
2022-11-28 21:53:01 +0800

    Update Program.cs
    -add comment to explain code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿/*
-Code File describe: Program interface
+/*
+Complete code: https://github.com/Dukebobo/FOP_FinalsOutput_Inventorysystem
+
+Code File describe:  Main method in program.cs;
 Project: FOP finals output
 Group: Group 3
 Submit Date: 5:22 PM, Nov 18, 2022 
+
 Renfrence: https://www.youtube.com/watch?v=az8mHaJXpRE
 */
+
 using ConsoleApp1;
 using System;
 
@@ -15,8 +19,10 @@ namespace Inventorysystem
         public static void Main(string[] args)
         {
 
-            Console.Title = "Inventory system Login";
-            login.loginGate();
+            Console.Title = "Inventory system";  //define console application windows title
+            
+            login.loginGate(); // login method 
+            
             Console.ReadKey();
         
         
